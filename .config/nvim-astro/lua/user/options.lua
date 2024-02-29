c32b55dcce27f1f0ff9b8dd1f1f4ea455906221e
Hussein-Mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2024-02-29 10:48:11 +0200

    feat: updated nvim config
    - added markdown preview plugin
    - added zen mode plugin
    - added todo comments
    - updated options
    - updated mapping for aerial

--- .config/nvim-astro/lua/user/options.lua
+++ .config/nvim-astro/lua/user/options.lua
@@ -7,6 +7,9 @@ return {
     spell = false, -- sets vim.opt.spell
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
+    colorcolumn = "80",
+    scrolloff = 4,
+    -- opt.undodir = os.getenv('HOME') .. "/.vim/undodir"
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
--- .config/nvim-astro/lua/user/plugins/community.lua
+++ .config/nvim-astro/lua/user/plugins/community.lua
@@ -4,22 +4,31 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
+  -- colorscheme
   { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.pack.bash" },
 
+  -- langs
+  { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.lua" },
 
+  -- web dev
   { import = "astrocommunity.pack.html-css" },
   { import = "astrocommunity.pack.tailwindcss" },
   { import = "astrocommunity.pack.typescript" },
 
+  -- config files
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.toml" },
   { import = "astrocommunity.pack.yaml" },
 
+  -- writing
   { import = "astrocommunity.pack.typst" },
   { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+
+  { import = "astrocommunity.editing-support.zen-mode-nvim" },
+  { import = "astrocommunity.editing-support.todo-comments-nvim" },
 }
