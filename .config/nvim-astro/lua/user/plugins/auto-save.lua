d56ae05ba35105943d9235503f2ec01879775a7d
Hussein-Mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2024-03-01 10:05:54 +0200

    fix: disabled auto save
    It cause screen to flash with auto format on save

--- .config/nvim-astro/lua/user/plugins/auto-save.lua
+++ .config/nvim-astro/lua/user/plugins/auto-save.lua
@@ -1,7 +1,8 @@
 return {
-  "pocco81/auto-save.nvim",
-  lazy = false,
-  opts = {
-    vim.api.nvim_set_keymap("n", "<leader>W", ":ASToggle<CR>", { desc = "Toggle auto save" }),
-  },
+  --   "pocco81/auto-save.nvim",
+  --   lazy = false,
+  --   opts = {
+  --     vim.api.nvim_set_keymap("n", "<leader>W", ":ASToggle<CR>", { desc = "Toggle auto save" }),
+  --   },
+  --   enabled = false,
 }
