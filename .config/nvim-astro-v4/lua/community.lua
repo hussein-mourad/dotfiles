48ca773be19681859eebe11653e8c3daad41687b
Hussein-Mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2024-05-11 09:46:06 +0300

    feat: migrated community plugins

--- .config/nvim-astro-v4/lua/community.lua
+++ .config/nvim-astro-v4/lua/community.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
@@ -7,6 +7,52 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
+  -- { import = "astrocommunity.recipes.vscode" },
+
+  -- colorscheme
+  { import = "astrocommunity.colorscheme.catppuccin" },
+
+  -- langs
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.lua" },
+
+  -- web dev
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.tailwindcss" },
+
+  -- config files
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.yaml" },
+
+  -- writing
+  { import = "astrocommunity.pack.typst" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+
+  { import = "astrocommunity.editing-support.zen-mode-nvim" },
+  { import = "astrocommunity.editing-support.todo-comments-nvim" },
+
+  -- motion
+  { import = "astrocommunity.motion.mini-surround" },
+
+  -- fuzzy-finder
+  { import = "astrocommunity.fuzzy-finder.telescope-zoxide" },
+
+  -- remote-development
+  { import = "astrocommunity.remote-development.netman-nvim" },
+
+  -- media
+  -- { import = "astrocommunity.media.image-nvim" },
+
+  -- completion
+  { import = "astrocommunity.completion.copilot-lua" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
