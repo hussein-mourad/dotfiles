06203bf7b51c428ad0dac7d11637ca5cc68ae916
hussein-mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2025-02-02 21:49:34 +0200

    add git worktree plugin to nvim

---
+++ .config/nvim/lua/plugins/git-worktree.lua
@@ -0,0 +1,18 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- configure git-worktree with telescope
+return {
+  "polarmutex/git-worktree.nvim",
+  config = function()
+    require("git-worktree").setup()
+    require("telescope").load_extension "git_worktree"
+    -- vim.keymap.set("n", "<Leader>fd", "<cmd>Telescope git_worktree git_worktrees<CR>")
+    -- vim.keymap.set("n", "<Leader>fD", "<cmd>Telescope git_worktree create_git_worktree<CR>")
+    vim.keymap.set("n", "<Leader>fd", "<cmd>lua require('telescope').extensions.git_worktree.git_worktrees()<CR>")
+    vim.keymap.set(
+      "n",
+      "<Leader>gw",
+      "<cmd>lua require('telescope').extensions.git_worktree.create_git_worktree()<cr>",
+      { desc = "Create git worktree" }
+    )
+  end,
+}
