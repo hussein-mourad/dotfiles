0bf42b9d9c62c470af71b0e8986031c4f6c2b116
Hussein Mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2025-04-04 19:04:10 +0200

    feat: add a plugin that auto convert string to f-string or template strings

---
+++ .config/nvim/lua/plugins/nvim-puppeteer.lua
@@ -0,0 +1,5 @@
+-- Master of strings. Automatically convert strings to f-strings or template strings and back.
+return {
+  "chrisgrieser/nvim-puppeteer",
+  lazy = false, -- plugin lazy-loads itself. Can also load on filetypes.
+}
