11e82bd1613730f58f787b4312b4da9d07dbaa66
Hussein Mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2026-07-09 07:35:50 +0300

    feat: enable spell checking

--- .config/nvim/lua/plugins/astrocore.lua
+++ .config/nvim/lua/plugins/astrocore.lua
@@ -42,7 +42,8 @@ return {
       opt = { -- vim.opt.<key>
         relativenumber = true, -- sets vim.opt.relativenumber
         number = true, -- sets vim.opt.number
-        spell = false, -- sets vim.opt.spell
+        spell = true, -- sets vim.opt.spell
+        spelllang = { "en_us" },
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
         wrap = true, -- sets vim.opt.wrap
         colorcolumn = "80",
