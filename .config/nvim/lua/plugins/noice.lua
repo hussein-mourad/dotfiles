89baf774b10c49f9fec0f30d865cb5559d08e34a
Hussein Mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2026-07-09 07:37:04 +0300

    feat: config cmd bar to be in the middle of the screen

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 return {
   "folke/noice.nvim",
@@ -7,29 +7,29 @@ return {
   opts = function(_, opts)
     local utils = require "astrocore"
     return utils.extend_tbl(opts, {
-      lsp = {
-        signature = {
-          enabled = false,
-        },
-        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true,
-        },
-      },
-      routes = {
-        -- Hide annoying written popup message
-        -- Ref: https://github.com/folke/noice.nvim/wiki/A-Guide-to-Messages
-        {
-          filter = {
-            event = "msg_show",
-            kind = "",
-            find = "written",
-          },
-          opts = { skip = true },
-        },
-      },
+      -- lsp = {
+      --   signature = {
+      --     enabled = false,
+      --   },
+      --   -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+      --   override = {
+      --     ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      --     ["vim.lsp.util.stylize_markdown"] = true,
+      --     ["cmp.entry.get_documentation"] = true,
+      --   },
+      -- },
+      -- routes = {
+      --   -- Hide annoying written popup message
+      --   -- Ref: https://github.com/folke/noice.nvim/wiki/A-Guide-to-Messages
+      --   {
+      --     filter = {
+      --       event = "msg_show",
+      --       kind = "",
+      --       find = "written",
+      --     },
+      --     opts = { skip = true },
+      --   },
+      -- },
       presets = {
         bottom_search = false, -- use a classic bottom cmdline for search
         command_palette = false, -- position the cmdline and popupmenu together
@@ -39,63 +39,63 @@ return {
       },
     })
   end,
-  specs = {
-    {
-      "nvim-treesitter/nvim-treesitter",
-      opts = function(_, opts)
-        if opts.ensure_installed ~= "all" then
-          opts.ensure_installed = require("astrocore").list_insert_unique(
-            opts.ensure_installed,
-            { "bash", "markdown", "markdown_inline", "regex", "vim" }
-          )
-        end
-      end,
-    },
-    {
-      "AstroNvim/astrolsp",
-      optional = true,
-      ---@param opts AstroLSPOpts
-      opts = function(_, opts)
-        local noice_opts = require("astrocore").plugin_opts "noice.nvim"
-        -- disable the necessary handlers in AstroLSP
-        if not opts.lsp_handlers then opts.lsp_handlers = {} end
-        if vim.tbl_get(noice_opts, "lsp", "hover", "enabled") ~= false then
-          opts.lsp_handlers["textDocument/hover"] = false
-        end
-        if vim.tbl_get(noice_opts, "lsp", "signature", "enabled") ~= false then
-          opts.lsp_handlers["textDocument/signatureHelp"] = false
-          if not opts.features then opts.features = {} end
-          opts.features.signature_help = false
-        end
-      end,
-    },
-    {
-      "heirline.nvim",
-      optional = true,
-      opts = function(_, opts)
-        local noice_opts = require("astrocore").plugin_opts "noice.nvim"
-        if vim.tbl_get(noice_opts, "lsp", "progress", "enabled") ~= false then -- check if lsp progress is enabled
-          opts.statusline[9] = require("astroui.status").component.lsp { lsp_progress = false }
-        end
-      end,
-    },
-    {
-      "folke/edgy.nvim",
-      optional = true,
-      opts = function(_, opts)
-        if not opts.bottom then opts.bottom = {} end
-        table.insert(opts.bottom, {
-          ft = "noice",
-          size = { height = 0.4 },
-          filter = function(_, win) return vim.api.nvim_win_get_config(win).relative == "" end,
-        })
-      end,
-    },
-    {
-      "catppuccin",
-      optional = true,
-      ---@type CatppuccinOptions
-      opts = { integrations = { noice = true } },
-    },
-  },
+  -- specs = {
+  --   {
+  --     "nvim-treesitter/nvim-treesitter",
+  --     opts = function(_, opts)
+  --       if opts.ensure_installed ~= "all" then
+  --         opts.ensure_installed = require("astrocore").list_insert_unique(
+  --           opts.ensure_installed,
+  --           { "bash", "markdown", "markdown_inline", "regex", "vim" }
+  --         )
+  --       end
+  --     end,
+  --   },
+  --   {
+  --     "AstroNvim/astrolsp",
+  --     optional = true,
+  --     ---@param opts AstroLSPOpts
+  --     opts = function(_, opts)
+  --       local noice_opts = require("astrocore").plugin_opts "noice.nvim"
+  --       -- disable the necessary handlers in AstroLSP
+  --       if not opts.lsp_handlers then opts.lsp_handlers = {} end
+  --       if vim.tbl_get(noice_opts, "lsp", "hover", "enabled") ~= false then
+  --         opts.lsp_handlers["textDocument/hover"] = false
+  --       end
+  --       if vim.tbl_get(noice_opts, "lsp", "signature", "enabled") ~= false then
+  --         opts.lsp_handlers["textDocument/signatureHelp"] = false
+  --         if not opts.features then opts.features = {} end
+  --         opts.features.signature_help = false
+  --       end
+  --     end,
+  --   },
+  --   {
+  --     "heirline.nvim",
+  --     optional = true,
+  --     opts = function(_, opts)
+  --       local noice_opts = require("astrocore").plugin_opts "noice.nvim"
+  --       if vim.tbl_get(noice_opts, "lsp", "progress", "enabled") ~= false then -- check if lsp progress is enabled
+  --         opts.statusline[9] = require("astroui.status").component.lsp { lsp_progress = false }
+  --       end
+  --     end,
+  --   },
+  --   {
+  --     "folke/edgy.nvim",
+  --     optional = true,
+  --     opts = function(_, opts)
+  --       if not opts.bottom then opts.bottom = {} end
+  --       table.insert(opts.bottom, {
+  --         ft = "noice",
+  --         size = { height = 0.4 },
+  --         filter = function(_, win) return vim.api.nvim_win_get_config(win).relative == "" end,
+  --       })
+  --     end,
+  --   },
+  --   {
+  --     "catppuccin",
+  --     optional = true,
+  --     ---@type CatppuccinOptions
+  --     opts = { integrations = { noice = true } },
+  --   },
+  -- },
 }
