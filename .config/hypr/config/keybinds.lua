29c2e98668a7dfa6dbdc9b7977cb28ee409d0e03
Hussein Mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2026-07-22 12:42:24 +0300

    refactor: rename scratchpad from magic to s

--- .config/hypr/config/keybinds.lua
+++ .config/hypr/config/keybinds.lua
@@ -71,10 +71,10 @@ end
 -- Fullscreen
 hl.bind(mod .. " + F11", hl.dsp.window.fullscreen({ mode = 0 }))
 
--- Example special workspace (scratchpad)
-hl.bind(mod .. " + S", hl.dsp.workspace.toggle_special("magic"))
-hl.bind(mod .. " + comma", hl.dsp.workspace.toggle_special("magic"))
-hl.bind(mod .. " + SHIFT + S", hl.dsp.window.move({ workspace = "special:magic" }))
+-- Example special workspace (scratchpad) (magic workspace)
+hl.bind(mod .. " + S", hl.dsp.workspace.toggle_special("s"))
+hl.bind(mod .. " + comma", hl.dsp.workspace.toggle_special("s"))
+hl.bind(mod .. " + SHIFT + S", hl.dsp.window.move({ workspace = "special:s" }))
 
 -- Scroll through existing workspaces with mainMod + scroll
 hl.bind(mod .. " + mouse_down", hl.dsp.focus({ workspace = "e+1" }))
