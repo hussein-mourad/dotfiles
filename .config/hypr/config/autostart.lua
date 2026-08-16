18fe9de37e4b14b6476c6de250558b35a77097be
Hussein Mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2026-08-17 01:19:38 +0300

    feat: add battery notifications

--- .config/hypr/config/autostart.lua
+++ .config/hypr/config/autostart.lua
@@ -25,6 +25,7 @@ hl.on("hyprland.start", function()
 	hl.exec_cmd("hypridle")
 	hl.exec_cmd("hyprpaper")
 	hl.exec_cmd("hyprsunset")
+	hl.exec_cmd("~/.local/bin/battery-notify")
 	hl.exec_cmd("flameshot")
 	hl.exec_cmd("wl-clip-persist --clipboard regular")
 
